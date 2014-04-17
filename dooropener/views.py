b88dce07834b27e1d0ef0a5851ad172d6aec5210
Braydn <braydn@Troy-Evanss-Mac-Pro-3.local>
2014-04-17 16:53:21 -0600

    Added ability to turn on/off movie room lights.

--- dooropener/views.py
+++ dooropener/views.py
@@ -1,3 +1,4 @@
+import subprocess
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
@@ -11,8 +12,11 @@ def text_from_twilio(request, *args, **kwargs):
         return HttpResponseNotFound("Nice try!")
     else:
         if smsbody == 'Activate':
-            #activate garage
             pass
+        elif smsbody == 'Bmf On':
+            subprocess.call(['/Library/Application\ Support/Perceptive\ Automation/Indigo\ 5/IndigoPluginHost.app/Contents/MacOS/IndigoPluginHost --port=1199 -e "indigo.device.turnOn(\'Basement Media Fluorescent\')"'], shell=True)
+        elif smsbody == 'Bmf Off':
+            subprocess.call(['/Library/Application\ Support/Perceptive\ Automation/Indigo\ 5/IndigoPluginHost.app/Contents/MacOS/IndigoPluginHost --port=1199 -e "indigo.device.turnOff(\'Basement Media Fluorescent\')"'], shell=True)
     return HttpResponse("Thanks!")
 
 
